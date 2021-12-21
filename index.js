9c0bedb9b166ad10fad6bd3cb83f7c14fc9adc31
valvegan <valentinavaccari@hotmail.com>
2021-12-21 15:03:20 +0000

    using express, morgan, and error handling in index.js file
    -GET route located at the endpoint “/movies” that returns a JSON object containing data about my top 10 movies.
    - GET route located at the endpoint “/” that returns a default textual response
    -express.static to serve “documentation.html” file from the public folder
    - Morgan middleware library to log all requests
    -error-handling middleware function

--- index.js
+++ index.js
@@ -1,2 +1,60 @@
-const express = require('express');
+const express = require('express'),
+  morgan = require('morgan');
 const app = express();
+app.use(morgan('common'));
+
+
+let topMovies = [
+    {
+      title: 'Pulp Fiction',
+    },
+    {
+      title: 'Donnie Darko',
+    },
+    {
+      title: 'Pretty Woman',
+    },
+    {
+      title: 'Spirited Away',
+    },
+    {
+        title: 'Memento',
+    },
+    {
+        title: 'Good Will Hunting',
+    },
+    {
+        title: 'The Silence of the Lambs',
+    },
+    {
+        title: 'Django Unchained',
+    },
+    {
+        title: 'The Shining',
+    },
+    {
+        title: 'Call me by your name',
+    }
+  ];
+  
+  // GET requests
+  app.get('/movies', (req, res) => {
+    res.json(topMovies);
+  });
+  app.get('/', (req, res) => {
+    res.send('Welcome to my movie API! (myFlix app)');
+  });
+  //express.static
+  app.use('/staticFiles', express.static('public'));
+  
+  //error handling
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
+  
+  // listen for requests
+  app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+  });
+  
